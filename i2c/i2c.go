cc468b7ed4c95e02c89a5394554695c65e790800
Jani <sleepwalker.bg@gmail.com>
2017-07-13 18:09:03 +0300

    Removing useless comments.

--- i2c/i2c.go
+++ i2c/i2c.go
@@ -1,11 +1,3 @@
-// Package i2c provides low level control over the linux i2c bus.
-//
-// Before usage you should load the i2c-dev kernel module
-//
-//      sudo modprobe i2c-dev
-//
-// Each i2c bus can address 127 independent i2c devices, and most
-// linux systems contain several buses.
 package i2c
 
 import (
@@ -14,12 +6,10 @@ import (
 	"syscall"
 )
 
-// I2C represents a connection to an i2c device.
 type I2C struct {
 	rc *os.File
 }
 
-// New opens a connection to an i2c device.
 func NewI2C(addr uint8, bus int) (*I2C, error) {
 	f, err := os.OpenFile(fmt.Sprintf("/dev/i2c-%d", bus), os.O_RDWR, 0600)
 	if err != nil {
@@ -33,8 +23,6 @@ func NewI2C(addr uint8, bus int) (*I2C, error) {
 	return this, nil
 }
 
-// Write sends buf to the remote i2c device. The interpretation of
-// the message is implementation dependant.
 func (this *I2C) Write(buf []byte) (int, error) {
 	return this.rc.Write(buf)
 }
